df7498a0765159b339715b0cef7af7eb941f4d6a
PedroJr28 <pedrojunior81000@gmail.com>
2026-04-11 16:58:08 -0300

    Primeira parte
    Primeira parte do desafio 005

--- Desafio004/grafico.js
+++ Desafio004/grafico.js
@@ -1,4 +1,10 @@
 const ctx = document.querySelector('.grafico').getContext('2d');
 const meuGrafico = new Chart(ctx, {
     type: 'line', data: {
-        labels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex'],
+        labels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex'],datasets: [{
+            label: 'Desempenho Semanal',
+            data: [12, 19, 3, 5, 2], // Os valores do gráfico
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    },
